492327aa50221244c4cea1d9290a1177b07cae49
Burak AKSU <96burakaksu@gmail.com>
2023-02-07 00:29:47 +0300

    Add project files.

---
+++ MyBlog.Model/CustomResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Model
+{
+    public class CustomResponse
+    {
+        public bool Success { get; set; }
+        
+    }
+
+    public class CustomResponse<T>:CustomResponse where T : class
+    {
+        public T Data { get; set; }
+
+    }
+
+
+}
---
+++ Repository/ContentRepository.cs
@@ -0,0 +1,25 @@
+﻿using MyBlog.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Repository
+{
+    public class ContentRepository 
+    {
+        
+
+        public CustomResponse AddContent()
+        {
+            using IDbConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStrings"].ConnectionString);
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+
+        }
+    }
+}
